8ef4f2f862c192cb4c4ffd0e804aa3f521e917d1
Mateus Moura <mateus51moura@gmail.com>
2023-03-09 22:10:31 -0300

    Disable menu on home screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
-import { CategoriasPage } from '../categorias/categorias';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -8,7 +8,15 @@ import { CategoriasPage } from '../categorias/categorias';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
+  }
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
   }
 
   login() {
